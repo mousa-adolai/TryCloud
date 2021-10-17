9b9b7b4647d88fa260112498b79a92caf5758e77
Mousa Adolai <mousa.adolai@gmail.com>
2021-10-17 17:02:35 -0400

    made properties file and ConfigReader and starting on Driver class

---
+++ src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream in = new FileInputStream("configuration.properties");
+            properties.load(in);
+        }catch (IOException e){
+            System.out.println("e.getMessage() = " + e.getMessage());
+        }
+    }
+
+    public String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+
+    private WebDriver driver;
+
+    private Driver(){}
+
+    public WebDriver getDriver(){
+        String browser ="";//config reader first
+        return null;
+
+    }
+}
