e71211397628bf16d0cc2d0c3bc489af43e9f105
Mousa Adolai <mousa.adolai@gmail.com>
2021-10-20 09:31:05 -0400

    update navigation class

--- src/test/java/com/cydeo/step_definitions/NavigationStepsDef.java
+++ src/test/java/com/cydeo/step_definitions/NavigationStepsDef.java
@@ -1,23 +1,22 @@
 package com.cydeo.step_definitions;
 
-import com.cydeo.pages.Navigation;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 
 import static com.cydeo.utilities.Driver.getDriver;
 import static org.junit.Assert.assertEquals;
+import static com.cydeo.pages.Navigation.goToLink;
 
 public class NavigationStepsDef {
-    Navigation nav = new Navigation();
 
     @And("user clicks on {string}")
     public void userClicksOn(String str) {
-        nav.goToLink(str);
+        goToLink(str);
     }
 
     @Then("user should be on the {string} page")
     public void userShouldBeOnThePage(String str) {
-        String expected_page = str.substring(0, 1).toUpperCase() + str.substring(1) + " - Trycloud QA";
+        String expected_page = str + " - Trycloud QA";
         System.out.println(expected_page);
         assertEquals(expected_page, getDriver().getTitle());
     }
